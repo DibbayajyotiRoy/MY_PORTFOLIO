4d1d0fd63a366ba31e75f3bc1972324bc8d1c336
Dibbayajyoti <125145390+DibbayajyotiRoy@users.noreply.github.com>
2023-12-03 00:02:14 +0530

    Landing page

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-var tl = gsap.timeline()
+var tl = gsap.timeline();
 
-tl
-.to("#fs",{
-    height: 0,
-    duration: 2,
-    ease: Expo.easeInOut,
+tl.to("#fs", {
+  height: 0,
+  duration: 2,
+  ease: Expo.easeInOut,
 })
 
-.to("#elem",{
+  .to("#elem", {
     height: "100%",
     duration: 2,
     delay: -2,
     ease: Expo.easeInOut,
-})
+  })
+
+  .to("#white", {
+    height: "100%",
+    duration: 2,
+    delay: -1.6,
+    ease: Expo.easeInOut,
+  });
+
+document
+  .querySelectorAll(".reveal")
+
+  .forEach(function (elem) {
+    let spanParent = document.createElement("span");
+    let spanChild = document.createElement("span");
+
+    spanParent.classList.add("parent");
+    spanChild.classList.add("child");
+
+    spanChild.textContent = elem.textContent;
+    spanParent.appendChild(spanChild);
+  });
