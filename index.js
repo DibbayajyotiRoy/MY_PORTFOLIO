28ee5efcc5edbc84a9adb0d2e614d89a2de307cf
Dibbayajyoti <125145390+DibbayajyotiRoy@users.noreply.github.com>
2023-12-03 11:11:31 +0530

    loading page II

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
+function revealToSpan() {
+  document.querySelectorAll(".reveal").forEach(function (elem) {
+    // create two spans
+    var parent = document.createElement("span");
+    var child = document.createElement("span");
+
+    // parent and child both sets their respective classes
+    parent.classList.add("parent");
+    child.classList.add("child");
+
+    // span parent gets child and child gets elem details
+    child.innerHTML = elem.innerHTML;
+    parent.appendChild(child);
+
+    // elem replaces it's value with parent span
+    elem.innerHTML = "";
+    elem.appendChild(parent);
+  });
+}
+
+revealToSpan();
+
 var tl = gsap.timeline();
 
-tl.to("#fs", {
-  height: 0,
+tl.from(".child span", {
+  x: 100,
+  stagger: .2,
   duration: 2,
-  ease: Expo.easeInOut,
-})
+  delay: 1,
+  ease: Power3.easeInOut,
+});
 
-  .to("#elem", {
-    height: "100%",
-    duration: 2,
-    delay: -2,
-    ease: Expo.easeInOut,
-  })
-
-  .to("#white", {
-    height: "100%",
-    duration: 2,
-    delay: -1.6,
-    ease: Expo.easeInOut,
-  });
-
-document
-  .querySelectorAll(".reveal")
-
-  .forEach(function (elem) {
-    let spanParent = document.createElement("span");
-    let spanChild = document.createElement("span");
-
-    spanParent.classList.add("parent");
-    spanChild.classList.add("child");
-
-    spanChild.textContent = elem.textContent;
-    spanParent.appendChild(spanChild);
-  });
+tl.to(".parent .child", {
+  y: "-100%",
+  duration: 1,
+  ease: Circ.easeInOut,
+});
